43331bd3351301e8ff28bb62f39067c5be4a5bb0
Juan Ortiz <juan.ortiz@thirdwavellc.com>
2014-10-30 16:50:21 -0500

    initial commit

---
+++ seed.rb
@@ -0,0 +1,10 @@
+DATA = [["ELECTRONICS", 1, 20],
+["TELEVISIONS", 2 , 9],
+["TUBE", 3 , 4],
+["LCD", 5 , 6],
+["PLASMA", 7 , 8],
+["PORTABLE ", 10, 19],
+["MP3 PLAYERS", 11, 14],
+["FLASH", 12, 13],
+["CD PLAYERS", 15, 16],
+["2 WAY RADIOS", 17, 18]]
